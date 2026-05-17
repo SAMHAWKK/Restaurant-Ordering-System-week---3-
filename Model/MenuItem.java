f520fcf85f8a3c21fb66e7f43906e8c0039c395a
SAMUEL MESERET <samiawkcole@gmail.com>
2026-05-17 16:17:24 +0300

    Create MenuItem.java

---
+++ Model/MenuItem.java
@@ -0,0 +1,35 @@
+package restaurant.models;
+
+// Abstraction
+public abstract class MenuItem {
+
+    // Encapsulation
+    private String name;
+    private double price;
+
+    // Constructor
+    public MenuItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    // Abstract method
+    public abstract void displayItem();
+}
